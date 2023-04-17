c4a7bf1e99a5a9d53e5826dd2307189df195d637
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-17 22:23:28 +0530

    Created a form space and the search field

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div>Search</div>
+        <div className='ui segment' style={{ maxWidth: '80%' }}>
+            <h1>Search</h1>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Type your query</label>
+                    <input className='input' placeholder='Search...' />
+                </div>
+            </div>
+        </div >
     )
 }
 
