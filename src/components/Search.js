d881407e69ab5790ebe7c55122cebad31f86e293
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-17 22:32:44 +0530

    Made the search bar a controlled input field

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Search = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    console.log(searchTerm);
     return (
         <div className='ui segment' style={{ maxWidth: '80%' }}>
             <h1>Search</h1>
             <div className='ui form'>
                 <div className='field'>
                     <label>Type your query</label>
-                    <input className='input' placeholder='Search...' />
+                    <input className='input' onChange={(st) => { setSearchTerm(st.target.value) }} value={searchTerm} placeholder='Search...' />
                 </div>
             </div>
         </div >
