1100cbbb2272954056d1f66eff642f56aafe2a39
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-17 22:07:17 +0530

    Created functional component for Search page and a decent nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import Accordion from "./components/accordion";
 import items from "./data/widget-data";
+import Search from "./components/Search";
 
 
 const App = () => {
+  const [activepage, setActivepage] = useState('accordion');
+
   return (
     <div className="App">
       <center>
-        <Accordion items={items} />
+        <button onClick={() => { setActivepage('accordion') }} >Accordion</button>
+        <button onClick={() => { setActivepage('search') }} >Search</button>
+        <div>
+          {activepage == 'search' ? <Search /> : <Accordion items={items} />}
+        </div>
       </center>
     </div>
   );
