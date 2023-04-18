7b0fcf26ccaa2abe51475c5e25becc53b85d0ea5
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-18 16:00:21 +0530

    rendered title and description of the search results on the search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,28 +3,45 @@ import axios from 'axios';
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [searchResult, setSearchResult] = useState({});
+    const [searchResult, setSearchResult] = useState([]);
 
     useEffect(() => {
         console.log("useeffect console log");
         (async () => {
             const result = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${searchTerm}`)
-            setSearchResult(result);
+            setSearchResult(result.data.query.search ? result.data.query.search : '');
             console.log(searchResult);
 
         })();
     }, [searchTerm])
 
-    return (
-        <div className='ui segment' style={{ maxWidth: '80%' }}>
-            <h1>Search</h1>
-            <div className='ui form'>
-                <div className='field'>
-                    <label>Type your query</label>
-                    <input className='input' onChange={(st) => { setSearchTerm(st.target.value) }} value={searchTerm} placeholder='Search...' />
+
+    const results = searchResult.map((r) => {
+        return (
+            <div className='ui list item' key={r.pageid} >
+                <div className='content'>
+                    <h5 className='header'>{r.title}</h5>
+                    {r.snippet}
                 </div>
             </div>
-        </div >
+        )
+    })
+
+    return (
+        <div>
+            <div className='ui segment' style={{ maxWidth: '80%' }}>
+                <h1>Search</h1>
+                <div className='ui form'>
+                    <div className='field'>
+                        <label>Type your query</label>
+                        <input className='input' onChange={(st) => { setSearchTerm(st.target.value) }} value={searchTerm} placeholder='Search...' />
+                    </div>
+                </div>
+            </div >
+            <div className='ui celled list' style={{ maxWidth: '80%' }} >
+                {results}
+            </div>
+        </div>
     )
 }
 
