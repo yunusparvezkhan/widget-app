e361bfc3cba8657e321748b86aebc0c2b878bb98
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-19 15:32:25 +0530

    Created the Dopdown.js file and routed it on the app.js navbar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import Accordion from "./components/accordion";
 import items from "./data/widget-data";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 
 const App = () => {
-  const [activepage, setActivepage] = useState('accordion');
+  const [activepage, setActivepage] = useState('dropdown');
 
   return (
     <div className="App">
       <center>
-        <button onClick={() => { setActivepage('accordion') }} >Accordion</button>
         <button onClick={() => { setActivepage('search') }} >Search</button>
+        <button onClick={() => { setActivepage('accordion') }} >Accordion</button>
+        <button onClick={() => { setActivepage('dropdown') }} >Dropdown</button>
         <div>
-          {activepage === 'search' ? <Search /> : <Accordion items={items} />}
+          {activepage === 'search' ? <Search /> : ''}
+          {activepage === 'accordion' ? <Accordion items={items} /> : ''}
+          {activepage === 'dropdown' ? <Dropdown /> : ''}
         </div>
       </center>
     </div>
