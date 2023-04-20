023c7df544ec63623afe3d351c9a7577cdbeb2ca
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-20 13:42:25 +0530

    Added the Dropdown component inside the Translate Page"

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dropdown from './Dropdown'
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    }
+]
 
 const Translate = () => {
+    const [selectedop, setSelectedop] = useState({ label: 'Select', value: '#202020' });
+
     return (
-        <div>Translate</div>
+        <div>
+            <Dropdown options={options} selectedop={selectedop} onSelectedopChange={setSelectedop} />
+        </div>
     )
 }
 
