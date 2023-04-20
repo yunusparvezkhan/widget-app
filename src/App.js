38f6dcfd72a966854257eb9952827f8bf0856e51
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-20 13:32:38 +0530

    Scaffolded the new component Translate, and routed in Nav Bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import items from "./data/widget-data";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import dropdownoptions from './data/Dropdown-data.js'
+import Translate from "./components/Translate";
 
 const App = () => {
   const [activepage, setActivepage] = useState('search');
@@ -16,10 +17,12 @@ const App = () => {
         <button onClick={() => { setActivepage('search') }} >Search</button>
         <button onClick={() => { setActivepage('accordion') }} >Accordion</button>
         <button onClick={() => { setActivepage('dropdown') }} >Dropdown</button>
+        <button onClick={() => { setActivepage('translate') }} >Translate</button>
         <div>
           {activepage === 'search' ? <Search /> : ''}
           {activepage === 'accordion' ? <Accordion items={items} /> : ''}
           {activepage === 'dropdown' ? <Dropdown options={dropdownoptions} selectedop={selectedop} onSelectedopChange={setSelectedop} /> : ''}
+          {activepage === 'translate' ? <Translate /> : ''}
         </div>
       </center>
     </div>
