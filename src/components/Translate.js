5609886ffcd9e03789f01971369dffe6aa0b0832
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-20 15:59:13 +0530

    v4 Delta 1.9
    Created a controlled input field inside Translate.js

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -19,10 +19,12 @@ const options = [
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
     const [bugfixingstate, setBugfixingstate] = useState('0px'); // Ignore this state, this one is made to keep up with the nroms of the Dropdown component. Also ignore the prop where this state setter is passed.
+    const [text, setText] = useState('');
 
     return (
         <div>
-            <div className='ui segment'>
+            <div className='ui segment translate-root'>
+                <input value={text} onChange={(e) => { setText(e.target.value) }} />
                 <Dropdown selectionlabel={'Select a language'} options={options} selectedop={language} onSelectedopChange={setLanguage} onColorboxtopChange={setBugfixingstate} />
             </div>
         </div>
