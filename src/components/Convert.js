cdabf75e2497e4c37d1ae52438088025ee8073e4
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-21 15:25:35 +0530

    v4 Delta 2.2
    Created Conversion functionalities. Initially build the functionality using useEffect hook, but due to too many post requests to a paid api, changed to a custome function, which runs after clicking on the particular button to translate.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,15 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-    useEffect(() => {
-        console.log('props in Convert.js has changed')
-    }, [language, text])
+
+    const [translatedtext, setTranslatedtext] = useState('');
+
+    // useEffect(() => {
+    //     axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+    //         params: {
+    //             q: text,
+    //             target: language.value,
+    //             key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+    //         }
+    //     })
+
+    //     console.log('props in Convert.js has changed')
+    // }, [language, text])
+
+
+
+    const converttext = async () => {
+        const res = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+            params: {
+                q: text,
+                target: language.value,
+                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            }
+        })
+        setTranslatedtext(res.data.data.translations[0].translatedText);
+        console.log(res.data.data.translations[0].translatedText)
+    }
+
     return (
 
         // This is not a form, put this classname for styling purposes only
         <div className='ui form'>
             <div className='field'>
                 <label className='label'>Output</label>
+                <button onClick={() => { converttext() }}> Convert Text into {language.label} </button>
+                <h3>{translatedtext}</h3>
             </div>
         </div>
     )
