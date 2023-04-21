4b49d2a7963ead703a24543bc72ecf92119bff07
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-21 16:01:16 +0530

    v4 Delta 2.6 * ⭐
    Removed previously used data loading functionality that was triggered by a button. See Commit v4 Delta 2.2

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -25,26 +25,11 @@ const Convert = ({ language, text }) => {
 
     }, [language, text])
 
-
-
-    // const converttext = async () => {
-    //     const res = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-    //         params: {
-    //             q: text,
-    //             target: language.value,
-    //             key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-    //         }
-    //     })
-    //     setTranslatedtext(res.data.data.translations[0].translatedText);
-    // }
-
     return (
-
         // This is not a form, put this classname for styling purposes only
         <div className='ui form'>
             <div className='field'>
                 <label className='label'>Output</label>
-                {/* <button> Convert Text into {language.label} </button> */}
                 <h2>{translatedtext}</h2>
             </div>
         </div>
