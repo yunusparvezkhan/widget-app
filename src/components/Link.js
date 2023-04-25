adb54c28fe6086d289b38a4459e701562c8f8a6c
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-25 22:52:32 +0530

    v5 Delta 1.6
    Added default behaviour on opening in a new tab option

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Link = ({ href, children }) => {
     const onClick = (event) => {
-        event.preventDefault();
-        window.history.pushState({}, '', href);
-
-        const navEvent = new PopStateEvent('popstate');
-        window.dispatchEvent(navEvent);
+        if (event.metakey || event.ctrlkey) {
+            return;
+        } else {
+            event.preventDefault();
+            window.history.pushState({}, '', href);
+            const navEvent = new PopStateEvent('popstate');
+            window.dispatchEvent(navEvent);
+        }
     }
     return (
-        <a onClick={onClick}>{children}</a>
+        <a href={href} onClick={onClick}>{children}</a>
     )
 }
 
